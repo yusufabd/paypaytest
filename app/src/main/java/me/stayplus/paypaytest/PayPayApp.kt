5d8cfcb7f179261c87c8589e37c32eda284e4d76
yusufabd <yusufabdullaev@gmail.com>
2020-12-06 08:23:04 +0500

    Initial commit

---
+++ app/src/main/java/me/stayplus/paypaytest/PayPayApp.kt
@@ -0,0 +1,11 @@
+package me.stayplus.paypaytest
+
+import android.app.Application
+
+class PayPayApp : Application(){
+
+    companion object {
+        const val API_KEY = "b99eb43af4ff1623d407e0dace610f53"
+    }
+
+}
